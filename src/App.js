1c24badf503982786bcdedc9ad1f45b1514e95f7
Raushan Sharma <raushankumar7485852533@gamil.com>
2024-08-30 22:05:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+ import { useMemo } from 'react';
 import './App.css';
+ import { useState } from 'react';
 
 function App() {
+  const [weight , setWeight] = useState(80)
+  const [height , setheight] = useState(150)
+
+  const onweightChange = (e) => {
+    setWeight(e.target.value);
+  } 
+
+const onheightChange = (e) => {
+  setheight(e.target.value);
+}
+
+const output = useMemo(() => {
+     const calculateheight = height/100;
+     const result = (weight / (calculateheight*calculateheight)) ;
+     return result.toFixed(1);
+},[weight,height]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <main>
+    <h1>BMI calculator</h1>
+    <div className='input_section'>
+        <p>Weight :  {weight}kg</p>
+        <input type="range" min='50' max='180' onChange={onweightChange}/>
+        <p>Height : {height} cm</p>
+        <input type="range" min='150' max='400' onChange={onheightChange} />
+
+        <p>your BMI is {output}</p>
     </div>
+   </main>
   );
 }
 
